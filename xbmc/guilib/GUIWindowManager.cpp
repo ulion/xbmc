7e47da69cd21b4cdbcba5ca92ec4a8dda7f0a524
ulion <ulion2002@gmail.com>
2013-02-17 23:53:25 +0800

    Add method to remove specified thread message in the message queue.

--- xbmc/guilib/GUIWindowManager.cpp
+++ xbmc/guilib/GUIWindowManager.cpp
@@ -825,6 +825,32 @@ void CGUIWindowManager::DispatchThreadMessages()
   }
 }
 
+int CGUIWindowManager::RemoveThreadMessageByMessageIds(int *pMessageIDList)
+{
+  CSingleLock lock(m_critSection);
+  int removedMsgCount = 0;
+  for (std::list <struct GUIThreadMessage_t>::iterator it = m_vecThreadMessages.begin();
+       it != m_vecThreadMessages.end();)
+  {
+    CGUIMessage *pMsg = it->pMsg;
+    int *pMsgID;
+    for(pMsgID = pMessageIDList; *pMsgID != 0; ++pMsgID)
+      if (pMsg->GetMessage() == *pMsgID)
+        break;
+    if (*pMsgID)
+    {
+      it = m_vecThreadMessages.erase(it);
+      delete pMsg;
+      ++removedMsgCount;
+    }
+    else
+    {
+      ++it;
+    }
+  }
+  return removedMsgCount;
+}
+
 void CGUIWindowManager::AddMsgTarget( IMsgTargetCallback* pMsgTarget )
 {
   m_vecMsgTargets.push_back( pMsgTarget );
